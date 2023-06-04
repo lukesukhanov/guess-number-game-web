c4c38476b00bc38b18a121da5e8c955e53daacb0
Luke Sukhanov <lukesukhanov@gmail.com>
2023-06-04 15:12:04 +0300

    Refactor

--- scripts/components/header/authorization/register-button.js
+++ scripts/components/header/authorization/register-button.js
@@ -1,15 +1,15 @@
 export { RegisterButton };
 
 class RegisterButton {
-	constructor(element) {
-		element.addEventListener("click", RegisterButton.handleClick);
-	}
+  constructor(element) {
+    element.addEventListener("click", RegisterButton.handleClick);
+  }
 
-	static handleClick(event) {
-		event.preventDefault();
-		const popUpBg = document.querySelector(".pop-up-bg");
-		const registrationForm = document.querySelector(".registration-form");
-		popUpBg.classList.add("active");
-		registrationForm.classList.add("active");
-	}
+  static handleClick(event) {
+    event.preventDefault();
+    const popUpBg = document.querySelector(".pop-up-bg");
+    const registrationForm = document.querySelector(".registration-form");
+    popUpBg.classList.add("active");
+    registrationForm.classList.add("active");
+  }
 }
